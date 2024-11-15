dafbb9ea9971d9f7226094da8db0391f1b835284
John Gabriel <166608691+SirJohnGabriel@users.noreply.github.com>
2024-11-15 19:07:09 +0800

    Update FunctionsAndErrors.sol

--- FunctionsAndErrors.sol
+++ FunctionsAndErrors.sol
@@ -19,25 +19,25 @@ contract FunctionsAndErrors {
     function logDistance(uint _distance) public {
         require(_distance > 0, "Distance must be greater than 0 kilometers");
         
-        uint256 previousDistance = healthRecords[msg.sender].distance;
+        uint previousDistance = healthRecords[msg.sender].distance;
         healthRecords[msg.sender].distance += _distance;
 
         assert(healthRecords[msg.sender].distance == previousDistance + _distance);
     }
 
-    function logCarbs(uint256 _carbs) public {
+    function logCarbs(uint _carbs) public {
         require(_carbs > 0, "Carbohydrate intake must be greater than 0 grams");
 
-        uint256 previousCarbs = healthRecords[msg.sender].carbs;
+        uint previousCarbs = healthRecords[msg.sender].carbs;
         healthRecords[msg.sender].carbs += _carbs;
 
         assert(healthRecords[msg.sender].carbs == previousCarbs + _carbs);
     }
 
-    function logSteps(uint256 _steps) public {
+    function logSteps(uint _steps) public {
         require(_steps > 0, "Steps must be greater than 0");
 
-        uint256 previousSteps = healthRecords[msg.sender].steps;
+        uint previousSteps = healthRecords[msg.sender].steps;
         healthRecords[msg.sender].steps += _steps;
 
         assert(healthRecords[msg.sender].steps == previousSteps + _steps);

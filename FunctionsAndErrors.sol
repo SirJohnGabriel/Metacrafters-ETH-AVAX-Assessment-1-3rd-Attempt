9695db13b19b397238a91222035764e7179ece53
John Gabriel <166608691+SirJohnGabriel@users.noreply.github.com>
2024-11-15 23:52:54 +0800

    Update FunctionsAndErrors.sol

--- FunctionsAndErrors.sol
+++ FunctionsAndErrors.sol
@@ -7,13 +7,19 @@ contract FunctionsAndErrors {
         uint distance; // km
         uint carbs; //gm
         uint steps;
+        uint sex; // 1 male, 2 female
+        uint age;
     }
 
     mapping(address => healthData) private healthRecords;
 
-    function setName(string memory _name) public {
+    function setUser(string memory _name, uint _sex, uint _age) public {
         require(bytes(_name).length > 0, "Name cannot be empty");
+        require(_sex > 0 && _sex < 3, "Sex must be only 1 for Male, and 2 for Female");
+        require(_age > 17, "You must be 18 years old and above");
         healthRecords[msg.sender].name = _name;
+        healthRecords[msg.sender].sex = _sex;
+        healthRecords[msg.sender].age = _age;
     }
 
     function logDistance(uint _distance) public {
@@ -58,5 +64,22 @@ contract FunctionsAndErrors {
     function getHealthRecord() public view returns (string memory, uint, uint, uint) {
         healthData memory data = healthRecords[msg.sender];
         return (data.name, data.distance, data.carbs, data.steps);
+    }
+
+    function calcWeightLost(uint _walkTime, uint _weight) public view returns (uint, uint ) {
+        require(_walkTime > 0, "Walk time must be greater than 0 minutes");
+        require(_weight > 0, "Weight must be greater than 0 kilograms");
+
+        uint MET = (healthRecords[msg.sender].sex == 1) ? 4 : 3; 
+        uint minutesInHour = 60;
+        uint kcalPerKg = 7700;
+
+        uint durationInHours = _walkTime / minutesInHour;
+
+        uint calories = MET * _weight * durationInHours;
+
+        uint gramsLost = (calories * 1000) / kcalPerKg;
+
+        return (calories, gramsLost);
     }
 }
